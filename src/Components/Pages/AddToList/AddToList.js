1e916811b6ef273801d103a9bb2828f314b11aac
wd-mahin <wdmahin.mhm@gmail.com>
2022-12-10 08:41:23 +0600

    added task submit method

--- src/Components/Pages/AddToList/AddToList.js
+++ src/Components/Pages/AddToList/AddToList.js
@@ -2,36 +2,52 @@ import React from 'react';
 
 const AddToList = () => {
 
+    const addTask = (e) => {
+        e.preventDefault();
+        const form = e.target;
+        const task = form.task.value;
+        const date = form.date.value;
+        const time = form.time.value;
+
+        const addedTask = {
+            task,
+            date,
+            time
+        }
+
+        console.log(addedTask);
+    }
+
     return (
         <div>
             <h2 className='text-3xl text-[#0074DD] font-bold py-5'>Add Tasks to list</h2>
 
-            <div className='lg:w-1/3 md:w-1/2 mx-auto border px-3 py-5 rounded-lg'>
+            <form onSubmit={addTask} className='lg:w-1/3 md:w-1/2 mx-auto border px-3 py-5 rounded-lg'>
                 <div className="form-control mb-3">
                     <label className='mb-2'>
                         <span className="text-xl text-start">Task that you want to add...</span>
                     </label>
-                    <input type="text" placeholder="Your Task" className="input input-bordered" />
+                    <input name='task' type="text" placeholder="Your Task" className="input input-bordered" />
                 </div>
 
                 <div className="form-control mb-3">
                     <label className='mb-2'>
                         <span className="text-xl text-start">Set Date</span>
                     </label>
-                    <input type="text" placeholder="Date" className="input input-bordered" />
+                    <input name='date' type="text" placeholder="Date" className="input input-bordered" />
                 </div>
 
                 <div className="form-control mb-3">
                     <label className='mb-2'>
                         <span className="text-xl text-start">Set Time</span>
                     </label>
-                    <input type="text" placeholder="Time (am/pm)" className="input input-bordered" />
+                    <input name='time' type="text" placeholder="Time (am/pm)" className="input input-bordered" />
                 </div>
 
                 <div className="form-control mb-3">
                     <input type="submit" value="Add Task" className='btn text-white text-xl font-semibold mt-10' />
                 </div>
-            </div>
+            </form>
         </div>
     );
 };
